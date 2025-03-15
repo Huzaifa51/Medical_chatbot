ab84549c0f669ae375a941c5bcb034bef972149e
huzaifa <huzaifajawed51@gmail.com>
2025-03-15 17:44:51 +0500

    Initial commit: Project structure and dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot",
+    author="Huzaifa",
+    author_email="huzaifajawed51@gmail.com",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[]
+)
